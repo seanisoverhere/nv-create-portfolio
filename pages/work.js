d79e2f3c8cc7613f7a9add36d14963abf8f43f17
seanisoverhere <sean_choon@hotmail.com>
2022-05-24 00:17:38 +0800

    Add project

--- pages/work.js
+++ pages/work.js
@@ -6,7 +6,25 @@ const Work = () => {
     <Layout>
       <div className="text-4xl text-center font-semibold pb-24">My Work</div>
       {/* Grid layout to set your projects */}
-      <div className=""></div>
+      <div className="">
+        <div>
+          <a
+            href="https://heartcode.scis.smu.edu.sg/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="object-cover w-full h-80 mb-6 rounded"
+              src="/placeholder.jpeg"
+              alt="Project HeartCode"
+            />
+          </a>
+          <p className="mb-2 text-xl font-bold leading-none sm:text-2xl">
+            Project 
+          </p>
+          <p>Sample Description of Project</p>
+        </div>
+      </div>
     </Layout>
   );
 };
