1480cf57214bdc12d32f45d5200e93a81a894fb4
seanisoverhere <sean_choon@hotmail.com>
2022-05-23 23:22:29 +0800

    Remove variants for framer

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,9 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
+  // hidden: { opacity: 0, x: -200, y: 0 },
+  // enter: { opacity: 1, x: 0, y: 0 },
+  // exit: { opacity: 0, x: 0, y: -100 },
 };
 
 const Layout = ({ children }) => {
